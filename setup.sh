283731948a30d79587eb74f131249cd86ffef2d9
Leonid Logvinov <logvinov.leon@gmail.com>
2021-09-06 09:34:13 +0000

    Add gitconfig to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
+git config --global user.email "logvinov.leon@gmail.com"
+git config --global user.name "Leonid Logvinov"
+
 ln -sf $(pwd)/vimrc $(pwd)/../.vimrc
+ln -sf $(pwd)/gitconfig $(pwd)/../.gitconfig
